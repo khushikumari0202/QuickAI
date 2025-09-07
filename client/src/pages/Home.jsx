e57fe256b1d8c4881a7830509a2b67fa0123e80a
khushikumari0202 <kk2682117@gmail.com>
2025-09-07 23:56:43 +0530

    work of day

--- client/src/pages/Home.jsx
+++ client/src/pages/Home.jsx
@@ -4,6 +4,7 @@ import Hero from '../components/Hero'
 import AiTools from '../components/AiTools'
 import Testimonials from '../components/Testimonials'
 import Plan from '../components/Plan'
+import Footer from '../components/Footer'
 
 const Home = () => {
   return (
@@ -13,6 +14,7 @@ const Home = () => {
       <AiTools />
       <Testimonials />
       <Plan />
+      <Footer />
     </>
   )
 }
