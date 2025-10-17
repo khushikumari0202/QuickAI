32803918fafc9f913d70112d1276224b2396b153
khushikumari0202 <kk2682117@gmail.com>
2025-10-17 13:20:09 +0530

    till now

--- client/src/pages/WriteArticle.jsx
+++ client/src/pages/WriteArticle.jsx
@@ -1,9 +1,21 @@
+import { Sparkles } from 'lucide-react'
 import React from 'react'
 
 const WriteArticle = () => {
   return (
-    <div>
-      <h1>Write Article</h1>
+    <div className='h-full overflow-y-scroll p-6 flex items-start flex-wrap
+    gap-4 text-slate-700'>
+      {/* left col */}
+      <form className='w-full max-w-lg p-4 bg-white rounded-bg-border border-gray-200'>
+        <div>
+          <Sparkles className='w-6 text-[#4a7aff]' />
+          <h1 className='text-xl font-semibold'>Article Configuration</h1>
+        </div>
+      </form>
+      {/* right col */}
+      <div>
+
+      </div>
     </div>
   )
 }
