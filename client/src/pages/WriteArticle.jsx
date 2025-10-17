8a50052edfb49c1747be728c10d241367c60c8f3
khushikumari0202 <kk2682117@gmail.com>
2025-10-17 13:57:02 +0530

    writeArticle page done

--- client/src/pages/WriteArticle.jsx
+++ client/src/pages/WriteArticle.jsx
@@ -1,20 +1,59 @@
-import { Sparkles } from 'lucide-react'
-import React from 'react'
+import { Edit, Edit2, Sparkles } from 'lucide-react'
+import React, { useState } from 'react'
 
 const WriteArticle = () => {
+
+  const articleLength = [
+    {length: 800, text: 'Short (500-800 words)'},
+    {length: 1200, text: 'Medium (800-1200 words)'},
+    {length: 1600, text: 'Long (1200+ words)'}
+  ]
+
+  const [selectedLength, setSelectedLength] = useState(articleLength[0])
+  const [input, setInput] = useState('')
+  const onSubmitHandler = async (e) => {
+    e.preventDefault();
+  }
+
   return (
     <div className='h-full overflow-y-scroll p-6 flex items-start flex-wrap
     gap-4 text-slate-700'>
       {/* left col */}
-      <form className='w-full max-w-lg p-4 bg-white rounded-bg-border border-gray-200'>
+      <form onClick={onSubmitHandler} className='w-full max-w-lg p-4 bg-white rounded-bg-border border-gray-200'>
         <div>
-          <Sparkles className='w-6 text-[#4a7aff]' />
-          <h1 className='text-xl font-semibold'>Article Configuration</h1>
+          <Sparkles className='w-5 text-[#4a7aff]' />
+          <h1 className='text-lg font-semibold'>Article Configuration</h1>
         </div>
+        <p className='mt-5 text-sm font-medium'>Article Topic</p>
+        <input onChange={(e) => setInput(e.target.value)} value={input} type='text' className='w-full p-2 px-3 mt-2 outline-none text-sm rounded-md border border-gray-300'
+         placeholder='The future of Artificial intelligence is ...' required/>
+         <p className='mt-4 text-sm font-medium'>Article Length</p>
+         <div className='mt-3 flex gap-3 flex-wrap sm:max-w-9/11'>
+          {articleLength.map((item, index) => (
+            <span onClick={() => setSelectedLength(item)} className={`text-xs px-4 py-1 border rounded-full cursor-pointer
+               ${selectedLength.text === item.text ? 'bg-blue-50 text-blue-700' : 'text-gray-500 border-gray-300'}`} key={index}>{item.text}</span>
+          ))}
+         </div>
+         <br />
+         <button className='w-full flex justify-center items-center gap-2 bg-gradient-to-r from-[#226bff] to-[#65adff] text-white px-4 py-2 mt-6 text-sm
+         rounded-lg cursor-pointer'>
+          <Edit className='w-4' />
+          Generate Article
+         </button>
       </form>
       {/* right col */}
-      <div>
-
+      <div className='w-full max-w-lg p-4 bg-white rounded-lg flex flex-col border border-gray-200 min-h-96 max-h-[600px]'>
+        <div className='flex items-center gap-2'>
+          <Edit className='w-5 h-5 text-[#4a7aff]' />
+          <h2 className='text-lg font-semibold'>Generated Article</h2>
+        </div>
+        <div className='flex-1 flex justify-center items-center'>
+          <div className='text-sm flex flex-col items-center gap-5 text-gray-400'>
+            <Edit className='w-5 h-5 text-[#4a7aff]' />
+            <p>No article generated yet. Start by entering a topic and clicking on "Generate Article"</p>
+          </div>
+        </div>
+          
       </div>
     </div>
   )
