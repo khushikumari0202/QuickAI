725e26186c20a2ce263b8e64c1a35b689447aa10
khushikumari0202 <kk2682117@gmail.com>
2025-10-22 01:02:57 +0530

    work

--- server/server.js
+++ server/server.js
@@ -2,8 +2,12 @@ import express from 'express';
 import cors from 'cors';
 import 'dotenv/config';
 import { clerkMiddleware, requireAuth } from '@clerk/express'
+import aiRouter from './routes/aiRoutes.js';
+import connectCloudinary from './configs/cloudinary.js';
 
 const app = express();
+
+await connectCloudinary()
 
 app.use(cors())
 app.use(express.json());
@@ -12,6 +16,8 @@ app.use(clerkMiddleware())
 app.get('/', (req, res) => res.send('Hello from QuickAI'))
 
 app.use(requireAuth())
+
+app.use('/api/ai', aiRouter)
 
 const PORT = process.env.PORT || 3000;
 app.listen(PORT, () => {
